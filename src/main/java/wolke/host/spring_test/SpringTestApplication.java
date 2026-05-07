c88fa3c437a7cf0811d1cbdfd8616eeef1cb5a77
dev-xindiri <eusebio.simango@xindiri.com>
2026-05-07 12:45:55 +0000

    hello world

--- src/main/java/wolke/host/spring_test/SpringTestApplication.java
+++ src/main/java/wolke/host/spring_test/SpringTestApplication.java
@@ -2,12 +2,24 @@ package wolke.host.spring_test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController // Adiciona isto
 public class SpringTestApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringTestApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTestApplication.class, args);
+    }
 
+    @GetMapping("/")
+    public String hello() {
+        return "Hello World from onwolke.app!";
+    }
+
+    @GetMapping("/healthz")
+    public String health() {
+        return "OK";
+    }
 }
